c621868673069874a987c11a307d5936ef2c7795
KonkeCanCode <konkegulwa02@gmail.com>
2025-01-24 18:45:08 -0800

    Made changes to theregistraton file

--- main.py
+++ main.py
@@ -2,14 +2,22 @@ import registration
 employee_list = []
 
 def main():
+
     print("Good Day!! Welcome to Employee Registration.")
+
     name = registration.employee_name()
+
     id_no = registration.employee_id_number()
+
     employee_id = registration.generate_employee_id(name,id_no)
-    print(f"Your auto generated Employee Id is {employee_id}. Make sure to remember this ID for future references. ")
+
+    print(f"Your auto-generated Employee Id is {employee_id}\n *Make sure to remember this ID for future references.* ")
+
     employee_list.append(employee_id)
+
     password = registration.generate_password(12)
-    print(f"Your auto generated password is: {password}.  Make sure to keep is safe and DO NOT share with anyone")
+
+    print(f"Your auto-generated password is: {password}\n *Make sure to keep it safe and DO NOT share with anyone.*")
 
 
 if __name__ == "__main__": 
